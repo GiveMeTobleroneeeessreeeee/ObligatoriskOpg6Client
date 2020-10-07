05c0b0af01982e8c6d4cdcab2e1b4f370e152e94
Tobias HH <58257308+GiveMeTobleroneeeessreeeee@users.noreply.github.com>
2020-10-08 00:23:29 +0200

    Add project files.

---
+++ ObligatoriskOpg6Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace ObligatoriskOpg6Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 4646);
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+
+                string s = string.Empty;
+                while (!s.Equals("Exit"))
+                {
+                    Console.Write("Enter a string to send to the server: ");
+                    s = Console.ReadLine();
+                    Console.WriteLine(s);
+                    writer.Flush();
+                    string server_string = reader.ReadLine();
+                    Console.WriteLine(server_string);
+
+                }
+                reader.Close();
+                writer.Close();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+        }
+    }
+}
